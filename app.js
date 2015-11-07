6fd66e665b8c8022fb6005206c1ac282acf53e8d
Patrick Heneise <patrick@fastmail.im>
2015-11-07 09:15:10 +0100

    express 4 starting point

--- app.js
+++ app.js
@@ -1,14 +1,100 @@
+'use strict';
+
 var express = require('express');
+var debug = require('debug')('gitmatch');
+var path = require('path');
+var http = require('http');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+var passport = require('passport');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+
 var app = express();
 
-//Create a static file server
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
+// view engine setup
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+
+if (app.get('env') === 'development') {}
+
+app.use(session({
+  store: new RedisStore(),
+  secret: 'Kn0ck0ut',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+passport.serializeUser(function(user, done) {
+  done(null, {
+    user: 'afinto'
+  });
 });
 
-//Get the dummy data
-require('./server/ddata.js');
+passport.deserializeUser(function(id, done) {
+  done(null, {
+    user: 'afinto'
+  });
+});
 
-var port = 8080;
-app.listen(port);
-console.log('Express server started on port %s', port);
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res) {
+    debug(err);
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+var server = http.createServer(app);
+server.listen(3000);
+server.on('error', function(error) {
+  console.log(error);
+});
