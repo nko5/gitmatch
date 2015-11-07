6d5c30b6fe9aa7c0e2eb316b5b49ea3e2491074e
Patrick Heneise <patrickheneise@fastmail.com>
2015-11-07 16:41:33 +0100

    fixed spacing and var

--- tests/repository.js
+++ tests/repository.js
@@ -4,12 +4,12 @@ var repository = require('../lib/repository');
 var badRepo = require('./data/repository_notready');
 
 test('Get package.json', function (t) {
-    t.plan(2);
+  t.plan(2);
 
-    repository.getPackageJson(badRepo).then(function(response) {
-      t.equal(typeof response, 'object');
-      t.equal(response.name, 'Gandalf');
-    }, function(error) {
-      t.end(error);
-    });
+  repository.getPackageJson(badRepo).then(function(response) {
+    t.equal(typeof response, 'object');
+    t.equal(response.name, 'Gandalf');
+  }, function(error) {
+    t.end(error);
+  });
 });
