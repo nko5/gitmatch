b0f5ae86d1f4cf3cb205159cfdb033261f3028a1
Moritz Sattler <moritz@moezalez.com>
2015-11-07 20:26:06 +0100

    Adding logic to query repos by user.

--- tests/search.js
+++ tests/search.js
@@ -1,12 +1,24 @@
 var test = require('tape');
 var search = require('../lib/search');
 
-test.only('Searching users on GitHub', function (t) {
+test('Searching users on GitHub', function (t) {
   t.plan(2);
   search.users('javascript', 'Barcelona')
   .then((users) => {
     t.ok(users);
     t.assert(users.length > 0);
+  })
+  .catch((error) => {
+    t.end(error);
+  });
+});
+
+test.only('Searching repos on GitHub', function (t) {
+  t.plan(2);
+  search.repos('octocat')
+  .then((repos) => {
+    t.ok(repos);
+    t.assert(repos.length > 0);
   })
   .catch((error) => {
     t.end(error);
