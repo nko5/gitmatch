f749a1ce451e04340fbfbf969682116b7706fe5f
Patrick Heneise <patrick@fastmail.im>
2015-11-08 19:02:34 +0100

    moved authenticated routes around to redirect to home once the session runs out

--- routes/index.js
+++ routes/index.js
@@ -1,72 +1,24 @@
 'use strict';
 
 var express = require('express');
-var passport = require('passport');
-var request = require('request');
 var repository = require('../lib/repository');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  if (req.user) {
-    res.redirect('/home');
-  } else {
-    res.render('index');
-  }
-});
-
-/* GET starts OAuth authorisation with Github */
-router.get('/auth', passport.authenticate('github'));
-
-/* GET authorisation error page. */
-router.get('/auth/error', function(req, res) {
-  res.render('loginko');
-});
-
-/* GET OAuth authorisation callback */
-router.get('/auth/callback',
-  passport.authenticate('github', {
-    failureRedirect: '/auth/error'
-  }),
-  function(req, res) {
-    res.redirect('/home');
-  }
-);
-
-/* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.render('about');
-});
-
-/* GET Logout */
-router.get('/logout', function(req, res, next) {
-  req.logout();
-  res.redirect('/');
-});
-
 /* GET fech all repos for the logged user */
-router.get('/home', function(req, res, next) {
-  var reposUrl = req.user.profile._json.repos_url;
-  var context = {
-    user: req.user
-  };
-
-  request.get({
-    url: reposUrl,
-    headers: {
-      'User-Agent': 'request'
-    },
-    json: true
-  }, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      context.repos = body;
-      res.render('home', context);
-    }
+router.get('/home', function(req, res) {
+  repository.getUserRepos(req.user.accessToken)
+  .then(function(resolved) {
+    var context = {};
+    context.repos = resolved;
+    res.render('home', context);
+  }, function(rejected) {
+    context.error = rejected;
+    res.render('home', context);
   });
 });
 
 /* GET checks the information for a repo */
-router.get('/check/:repo', function(req, res, next) {
+router.get('/check/:repo', function(req, res) {
   var context = {};
   var username = req.user.profile.username;
   var reponame = req.params.repo;
@@ -74,6 +26,7 @@ router.get('/check/:repo', function(req, res, next) {
   context.repo = reponame;
   repository.getRepo(username, reponame).then(function(repo) {
     repository.checkRepo(repo).then(function(result) {
+      console.log(result);
       res.redirect('/match/' + reponame);
     }, function(errors) {
       context.errors = errors;
@@ -84,13 +37,14 @@ router.get('/check/:repo', function(req, res, next) {
 });
 
 /* GET repo is valid so match process starts */
-router.get('/match/:repo', function(req, res, next) {
-  var username = req.user.profile.username;
-  var reponame = req.params.repo;
-  var context = {
-    user: req.user
-  };
-  res.render('match', context);
+router.get('/match/:repo', function(req, res) {
+  // TODO
+  // var username = req.user.profile.username;
+  // var reponame = req.params.repo;
+  // var context = {
+  //   user: req.user
+  // };
+  // res.render('match', context);
 });
 
 module.exports = router;
